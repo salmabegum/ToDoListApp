cb4909ba5078ba929af839d9c9bed495e710cd2a
Salma Begum <salma.bcse@hotmail.com>
2022-05-30 19:21:13 +1000

    Add project files.

---
+++ ToDoListApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoListApp.Models;
+
+namespace ToDoListApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly List<TodoItem> _todoItems = new List<TodoItem>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionRe                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    sult Index()
+        {
+            var item = new TodoItem
+            {
+                Id = 1,
+                Text = "Programming",
+                CreatedAt = DateTimeOffset.UtcNow,
+                IsCopmpleted = false
+            };
+            _todoItems.Add(item);
+            return View("Index",_todoItems);
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        } 
+        public IActionResult Privacy()
+        {
+            return View("Index",_todoItems);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ToDoListApp/Models/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set;  }
+         
+        [Required]
+         public string Text { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public bool IsCopmpleted { get; set; }
+
+
+    }
+}
